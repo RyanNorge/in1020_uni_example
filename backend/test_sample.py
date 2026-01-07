30ebb364e8cd809050ed6a61cd266f58e7322e73
RyanNorge <ryan@ryanburch.no>
2026-01-07 14:32:29 +0100

    Set up server test client

---
+++ backend/test_sample.py
@@ -0,0 +1,22 @@
+from fastapi.testclient import TestClient
+
+from server import app
+
+
+test_client = TestClient(
+    app=app,
+    base_url="http://testserver",
+    raise_server_exceptions=True,
+    root_path="",
+    backend="asyncio",
+    backend_options=None,
+    cookies=None,
+    headers=None,
+    follow_redirects=True,
+    client=("testclient", 50000),
+)
+
+
+def test_root():
+    response = test_client.get("/")
+    assert response.json() == {"Hello": "World"}
