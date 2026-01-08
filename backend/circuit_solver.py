614efffcbddf3ca749799c45ba05a8c36da1f43d
RyanNorge <ryan@ryanburch.no>
2026-01-08 19:35:29 +0100

    Further on circuit solver. Add string validation and formatting. Change from empty strings for leaves on CircuitSolver to single chars.

--- backend/circuit_solver.py
+++ backend/circuit_solver.py
@@ -1,20 +1,70 @@
+from typing import Literal
+from string import ascii_uppercase
+
+# using all caps is Python convention for declaring a "constant"
+# it's just like using "_" for a private variable. we just close our eyes and pretend :)
+
+# removing "X" as I'm going to have the user type it for an XOR gate
+NOR_LETTERS = ascii_uppercase.replace("X", "") + "ÆØÅ"
+
+VALID_CHARS = NOR_LETTERS + "()*+!X \n"
+
+
+def validate_string(string: str) -> bool:
+    # empty string
+    if not string:
+        return False
+
+    # invalid char in string
+    for char in set(string):
+        if char not in VALID_CHARS:
+            return False
+
+    # string is one char, and is not a letter
+    if len(string) == 1 and string not in NOR_LETTERS:
+        return False
+
+    # depth of parenthesis is not matching
+    if not string.count("(") == string.count(")"):
+        return False
+
+    return True
+
+
+def format_string(string: str) -> str:
+    string = string.upper()
+    string = string.replace(" ", "")
+    string = string.replace("\n", "")
+    return string
+
+
 class CircuitSolver:
     def __init__(self, circuit_string: str) -> None:
-        self._cir_string = circuit_string
-        self._operation: str | None = None
+        self._cir_string = circuit_string.upper()
+        self._invert = False
+        self._operation: Literal["and", "or", "xor"]
 
-        if self._cir_string == "":
-            return
+        # condition for a leaf. should be a single character (excluding "X")
+        if len(self._cir_string) == 1:
+            if self._cir_string in NOR_LETTERS:
+                return
+            else:
+                raise ValueError
 
-        if self._cir_string:
-            # TODO
+        if self._cir_string[0] == "!":
+            self._invert = True
+
+        # TODO this is only for ending nodes
+        if "+" in self._cir_string:
+            self._operation = "or"
+        else:
             self._operation = "and"
 
-        self._right = CircuitSolver("")
-        self._left = CircuitSolver("")
+        self._right = CircuitSolver("A")
+        self._left = CircuitSolver("B")
 
     def get_value(self) -> bool:
-        if self._cir_string == "":
+        if len(self._cir_string) == 1:
             return True
 
         left = self._left.get_value()
@@ -22,8 +72,18 @@ class CircuitSolver:
 
         match self._operation:
             case "and":
-                return left and right
+                value = left and right
             case "or":
-                return left or right
-            # case "not":
-            #     ...
+                value = left or right
+            case "xor":
+                raise NotImplementedError
+            case _:
+                raise ValueError
+
+        if self._invert:
+            value = not value
+
+        return value
+
+
+# NOTE i need to handle inversion for each bool. should use something other than empty string?
--- backend/test_circuit_solver.py
+++ backend/test_circuit_solver.py
@@ -1,16 +1,44 @@
-from circuit_solver import CircuitSolver
+from circuit_solver import CircuitSolver, validate_string, format_string
 
 s_blank = ""
 s_and = "AB"
+s_or = "A+B"
+s_not = "!A"
+
+
+def test_valid_strings():
+    assert validate_string("AB")
+    assert validate_string("A+B")
+    assert validate_string("A + B")
+    assert validate_string("(A + B) \n")
+    assert validate_string("(A + B) + C")
+    assert validate_string("!A")
+    assert validate_string("!Ø+Å")
+
+
+def test_invalid_strings():
+    assert not validate_string("A-B")
+    assert not validate_string("!")
+    assert not validate_string(" ")
+    assert not validate_string("")
+    assert not validate_string("(")
+    assert not validate_string("( ()")
+
+
+def test_format_string():
+    assert format_string("AB") == "AB"
+    assert format_string("ab") == "AB"
+    assert format_string(" a b \n") == "AB"
 
 
 def test_string_arg():
-    for arg in (s_and, s_blank):
+    for arg in (s_and, s_or):
         cir_solv = CircuitSolver(arg)
         assert cir_solv._cir_string == arg
 
 
 def test_blank():
+    return
     cir_solv = CircuitSolver(s_blank)
     assert cir_solv.get_value()
 
@@ -18,3 +46,14 @@ def test_blank():
 def test_and():
     cir_solv = CircuitSolver(s_and)
     assert cir_solv.get_value()
+
+
+def test_or():
+    cir_solv = CircuitSolver(s_or)
+    assert cir_solv.get_value()
+
+
+def test_not():
+    return  # TODO
+    cir_solv = CircuitSolver(s_not)
+    assert not cir_solv.get_value()
